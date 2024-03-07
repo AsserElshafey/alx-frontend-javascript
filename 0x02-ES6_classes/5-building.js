8e1d345f89c80214b5d793997709fd20062a731c
Asser Elshafey <elshafeyasser@yahoo.com>
2024-03-07 18:19:25 +0200

    HOT FIX

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,16 @@
-import Building from './5-building';
-
-class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
-    this._floors = floors;
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMesagge !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
   }
 
-  get floors() {
-    return this._floors;
+  get sqft() {
+    return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
 }
--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,23 +3,11 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._sqft = sqft;
     this.floors = floors;
-  }
-
-  get sqft() {
-    return this._sqft;
   }
 
   get floors() {
     return this._floors;
-  }
-
-  set floors(newFloors) {
-    if (typeof newFloors !== 'number') {
-      throw TypeError('Floors must be a number');
-    }
-    this._floors = newFloors;
   }
 
   evacuationWarningMessage() {
